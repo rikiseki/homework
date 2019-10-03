2a3c4f19f254e584da643132398a8f3c0e6345b1
KIDstark <51429420+KIDstark@users.noreply.github.com>
2019-10-03 10:20:39 +0800

    Add files via upload

---
+++ 4.3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int k = 1;
+	int a[10][10] = {0};
+	int x = 0, y = 0;
+	a[x][y] = 1;
+	while (n * n!=k)
+	{
+		while (y + 1 < n && !a[x][y + 1])
+			a[x][++y] = ++k;
+		while (x + 1 < n && !a[x+1][y])
+			a[++x][y] = ++k;
+		while (y -1 >= 0 && !a[x][y - 1])
+			a[x][--y] = ++k;
+		while (x-1>=0 && !a[x-1][y])
+			a[--x][y] = ++k;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
