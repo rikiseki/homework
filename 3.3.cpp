2c41363244a1961b1d82916b47e782f5830f4f13
KIDstark <51429420+KIDstark@users.noreply.github.com>
2019-10-19 22:32:00 +0800

    Add files via upload

---
+++ 3.3.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+typedef struct Node
+{
+	int num;
+	int score;
+	struct Node *next;
+}*Pointer;
+void print(Pointer head)
+{
+	Pointer p = head->next;
+	while (p != NULL)
+	{
+		cout << "[num="<<p->num<<",score="<<p->score<<"]"<<endl;
+		p = p->next;
+	}
+}
+int main()
+{
+	Pointer head=new Node;
+	head->next=NULL;
+	Pointer p=head;
+	int x,y;
+	for(int i=0;i<3;i++)
+	{
+		Node *k=new Node;
+		cin>>x>>y;
+		k->num=x;
+		k->score=y;
+		p->next=k;
+		k->next=NULL;
+		p=k;
+	}
+	print(head);
+	return 0;
+}
