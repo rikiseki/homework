fe3d00b6c82f6c489a6e4d16dd31d8c6e18f6db7
KIDstark <51429420+KIDstark@users.noreply.github.com>
2019-10-19 22:29:17 +0800

    Add files via upload

---
+++ 2.2(2).cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+#include<stack>
+#include<queue>
+int main()
+{
+	stack<int> s;
+	queue<int> q;
+	int n,x;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> x;
+		q.push(x);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		temp = q.front();
+		s.push(temp);
+		q.pop();
+	}
+	for (int i = 0; i < n; i++)
+	{
+		int temp;
+		temp = s.top();
+		q.push(temp);
+		s.pop();
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << q.front() << " ";
+		q.pop();
+	}
+
+}
+
